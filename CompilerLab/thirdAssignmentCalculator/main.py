a9b4c8e028fc44d56ca45df88e5f99cc65da0313
apl-mhd <mmahmud161036@bscse.uiu.ac.bd>
2019-04-02 22:08:25 +0600

    infix to postfix problem solved

---
+++ CompilerLab/thirdAssignmentCalculator/main.py
@@ -0,0 +1,4 @@
+import postfix
+
+
+print(postfix.inFixToPostFix('a+((b+c))*(d+c)'))
--- CompilerLab/thirdAssignmentCalculator/postfix.py
+++ CompilerLab/thirdAssignmentCalculator/postfix.py
@@ -11,8 +11,7 @@ stackOprator = [] #+,-*,/
 stackOprand = []
 
 
-if '(' in bracket and '+' in pm:
-    print('hello')
+
 
 
 
@@ -28,37 +27,36 @@ def inFixToPostFix(expr):
 
         elif i in pm and  len(stackOprator) != 0 and stackOprator[-1] in md or i in ')':
 
-            while(len(stackOprator) !=0):
+            while(len(stackOprator) != 0):
 
                     if stackOprator[-1] == '(':
                         stackOprator.pop()
+                        break
                     else:
                         stackOprand.append(stackOprator.pop())
 
         else:
             stackOprator.append(i)
 
-    print(stackOprator)
+
 
     while(len(stackOprator) !=0):
         stackOprand.append(stackOprator.pop())
 
+    return stackOprand
+
+
+
+
+#abc++dc+*
+#abc+dc+*+
+#abc+dc+*+
+#abc+dc+*+
 
 
 
 
 
-
-
-
-
-
-
-
-inFixToPostFix('a+(b*c)+x')
-
-print(stackOprand)
-print(stackOprator)
 
 
 
